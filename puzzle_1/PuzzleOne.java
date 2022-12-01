6a70319f383780972759f8b0ee4995ac0d0fc952
lRaulMN7 <raultelo@gmail.com>
2022-12-01 21:28:36 +0100

    part 2 puzzle 1

--- puzzle_1/PuzzleOne.java
+++ puzzle_1/PuzzleOne.java
@@ -2,28 +2,35 @@ package puzzle_1;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class PuzzleOne {
     public static void main(String[] args) throws IOException {
 
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
         File file = new File("puzzle_1", "elf_calories.txt");
 
         AtomicInteger currentCalories = new AtomicInteger(0);
-        AtomicInteger maxCalories = new AtomicInteger(0);
 
         Files.lines(file.toPath()).forEach(line -> {
             if (line.isEmpty() ) {
-                if(currentCalories.get() > maxCalories.get()) {
-                    maxCalories.set(currentCalories.get());
-                }
+                queue.add(currentCalories.get());
                 currentCalories.set(0);
             } else {
                 currentCalories.addAndGet(Integer.parseInt(line));
             }
         });
 
-        System.out.println(maxCalories.get());
+        int maxCalories = queue.poll();
+        int secondCalories = queue.poll();
+        int thirdCalories = queue.poll();
+
+        System.out.println("Part1:" + maxCalories);
+        System.out.println("Part2:" + (maxCalories + secondCalories + thirdCalories));
 
 
 
