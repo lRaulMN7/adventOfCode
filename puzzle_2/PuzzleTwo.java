14f6961456b9b7a4ecc90302b72b9b74cdb0407b
lRaulMN7 <raultelo@gmail.com>
2022-12-03 22:58:46 +0100

    part 2 puzzle 2

--- puzzle_2/PuzzleTwo.java
+++ puzzle_2/PuzzleTwo.java
@@ -7,44 +7,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PuzzleTwo {
-    static Map<Character, Map<Character, Integer>> scoreOutcome;
-    static Map<Character, Integer> scoreShape;
+    static Map<Character, Map<Character, Integer>> scoreOutcomeOne, scoreOutcomeTwo;
 
-    public static void init(){
-        scoreOutcome = new HashMap<>();
-        scoreOutcome.put('X', Map.of('A', 3,'B',0, 'C', 6));
-        scoreOutcome.put('Y', Map.of('A', 6, 'B', 3, 'C', 0));
-        scoreOutcome.put('Z', Map.of('A',0,'B', 6, 'C', 3));
+    public static void init() {
+        scoreOutcomeOne = new HashMap<>();
+        scoreOutcomeOne.put('X', Map.of('A', 4, 'B', 1, 'C', 7));
+        scoreOutcomeOne.put('Y', Map.of('A', 8, 'B', 5, 'C', 2));
+        scoreOutcomeOne.put('Z', Map.of('A', 3, 'B', 9, 'C', 6));
 
-        scoreShape = new HashMap<>();
-        scoreShape.put('X', 1);
-        scoreShape.put('Y', 2);
-        scoreShape.put('Z', 3);
+        scoreOutcomeTwo = new HashMap<>();
+        scoreOutcomeTwo.put('X', Map.of('A', 3, 'B', 1, 'C', 2));
+        scoreOutcomeTwo.put('Y', Map.of('A', 4, 'B', 5, 'C', 6));
+        scoreOutcomeTwo.put('Z', Map.of('A', 8, 'B', 9, 'C', 7));
     }
 
-    public static int computeScore(String elf, String human) {
-        int score = scoreOutcome.get(human.charAt(0)).get(elf.charAt(0));
-        int shape = scoreShape.get(human.charAt(0));
-        return score + shape;
-    }
-
-    public static void main(String[] args) throws IOException {
-        File file = new File("puzzle_2", "elf_strategy.txt"); //12855
+    public static void main(String[] args) {
+        File file = new File("puzzle_2", "elf_strategy.txt");
         init();
 
         try (var stream = Files.lines(file.toPath())) {
+            int[] solution = stream
+                    .map(line -> new int[]{
+                            scoreOutcomeOne.get(line.charAt(2)).get(line.charAt(0)),
+                            scoreOutcomeTwo.get(line.charAt(2)).get(line.charAt(0))
+                    })
+                    .reduce(new int[]{0, 0}, (a, b) -> new int[]{
+                            a[0] + b[0],
+                            a[1] + b[1]
+                    });
 
-            int solution = stream.map(line -> line.split(" "))
-                    .mapToInt(line -> computeScore(line[0], line[1]))
-                    .sum();
-
-            System.out.println("Total points: " + solution);
+            System.out.println("First part: " + solution[0]);
+            System.out.println("Second part: " + solution[1]);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
-
-
 }
